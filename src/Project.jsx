c7a1a6994eea972314ae5abf83f3af0ed10c9c15
DongHY1 <hi@daviddong.me>
2024-06-13 15:08:22 +0800

    feat: add biome as lint and formatter tool

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react"
+import { useState } from "react";
 export default function Project() {
-    const [counter, setCounter] = useState(0)
-    return (
-        <div>
-            <span>{counter}</span>
-            <button className="rounded-md bg-white p-2 text-slate-700" onClick={() => setCounter(counter + 1)}>Click</button>
-        </div>
-    )
+	const [counter, setCounter] = useState(0);
+	return (
+		<div>
+			<span>{counter}</span>
+			<button
+				type="button"
+				className="rounded-md bg-white p-2 text-slate-700"
+				onClick={() => setCounter(counter + 1)}
+			>
+				Click
+			</button>
+		</div>
+	);
 }

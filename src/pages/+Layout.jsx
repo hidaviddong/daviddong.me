9c67041811af71a9f4b41d13a18835995229f06f
David Dong <hi@daviddong.me>
2024-06-14 20:44:13 +0800

    update: about me

--- src/pages/+Layout.jsx
+++ src/pages/+Layout.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react";
 import "./index.css";
 import { motion } from "framer-motion";
+import Link from "../components/Link";
 export default function LayoutDefault({ children }) {
 	const [isOpen, setIsOpen] = useState(false);
 	return (
-		<div className="w-full h-screen selection:text-blue-500 selection:bg-blue-100">
-			<header className="w-full flex justify-center items-center">
-				{/* <a href="/">About</a>
-				<a href="/blog">Blog</a>
-				<a href="/project">Project</a> */}
-
+		<div className="w-full h-screen selection:text-blue-500 selection:bg-blue-100 bg-gradient-to-b from-zinc-100 to-zinc-50 flex flex-col items-center">
+			<main className="h-full flex justify-center items-center flex-col">{children}</main>
+			<motion.div
+				layout
+				data-isopen={isOpen}
+				initial={{ borderRadius: 50 }}
+				transition={{ duration: 0.3 }}
+				className="fixed bottom-4 data-[isopen=true]:h-48 backdrop-blur-sm w-96 h-12 flex flex-col justify-center items-center  bg-white/30 shadow-lg"
+				onHoverStart={() => setIsOpen(true)}
+				onHoverEnd={() => setIsOpen(false)}
+				onClick={() => setIsOpen(!open)}
+			>
 				<motion.div
 					layout
-					data-isopen={isOpen}
-					initial={{ borderRadius: 50 }}
-					transition={{ duration: 0.3 }}
-					className="data-[isopen=true]:w-48 w-24 h-12 flex justify-center items-center bg-black shadow-lg mt-4"
-					onHoverStart={() => setIsOpen(true)}
-					onHoverEnd={() => setIsOpen(false)}
+					className="text-black font-sans font-medium m-1 w-full flex justify-center items-center gap-x-4"
 				>
-					<motion.div layout className="text-white">
-						<a href="/blog">Blog</a>
-					</motion.div>
+					<Link href="/">About</Link>
+					<Link href="/blog">Blog</Link>
+					<Link href="/project">Project</Link>
 				</motion.div>
-			</header>
-			<main>{children}</main>
+			</motion.div>
 		</div>
 	);
 }

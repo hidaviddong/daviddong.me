1f1c1321f44fe73329e3ae2de302cfafbd5d4ecc
daviddong <hi@daviddong.me>
2024-06-16 21:35:46 +0800

    feat: about me

--- src/pages/index/+Page.jsx
+++ src/pages/index/+Page.jsx
@@ -1,19 +1,34 @@
 import { Badge } from "@/components/ui/badge";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-
 export default function Page() {
 	return (
-		<article className="prose prose-stone prose-p:text-neutral-500 p-12">
+		<article className="prose prose-p:text-neutral-500 p-12">
 			<h1>Hey, I'm Haoyu</h1>
 			<p>
 				You can call me David. I'm a software engineer and previously worked at
-				<Badge variant="outline" className="px-2 mx-1 text-sm">
-					Tencent Music
-				</Badge>
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger>
+							<Badge variant="outline" className="px-2 mx-1 text-sm">
+								{/* <a href="https://www.tencentmusic.com/en-us/">Tencent Music</a> */}
+								Tencent Music
+							</Badge>
+						</TooltipTrigger>
+						<TooltipContent> Intern Software Developer from May 2022 to Oct 2022 </TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
 				and
-				<Badge variant="outline" className="px-2 mx-1 text-sm">
-					Peking University Institute
-				</Badge>
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger>
+							<Badge variant="outline" className="px-2 mx-1 text-sm" asChild>
+								{/* <a href="https://icode.pku.edu.cn/">Peking University Institute</a> */}
+								Peking University Institute
+							</Badge>
+						</TooltipTrigger>
+						<TooltipContent> Junior Software Developer from Jul 2023 to Jul 2024 </TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
 				.
 			</p>
 			<p>
@@ -25,9 +40,7 @@ export default function Page() {
 								play guitar
 							</Badge>
 						</TooltipTrigger>
-						<TooltipContent>
-							R&B, Neo Soul, Gospel
-						</TooltipContent>
+						<TooltipContent>R&B, Neo Soul, Gospel</TooltipContent>
 					</Tooltip>
 				</TooltipProvider>
 				,and
@@ -38,9 +51,7 @@ export default function Page() {
 								watch movies
 							</Badge>
 						</TooltipTrigger>
-						<TooltipContent>
-							Thriller, Tragicomedy
-						</TooltipContent>
+						<TooltipContent>Thriller, Tragicomedy</TooltipContent>
 					</Tooltip>
 				</TooltipProvider>
 				. My favorite director is Woody Allen.

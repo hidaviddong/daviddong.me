7e228271f95bfce67d904cce82d4902c0d857a63
daviddong <hi@daviddong.me>
2024-06-16 22:52:20 +0800

    chore: lint

--- src/components/ui/tag.jsx
+++ src/components/ui/tag.jsx
@@ -1,30 +1,30 @@
-import { useMediaQuery } from '@react-hookz/web';
+import { useMediaQuery } from "@react-hookz/web";
 import { useState } from "react";
+import { Badge } from "./badge";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip";
-import { Badge } from './badge';
 function Tag({ text, tooltip }) {
-    const isSmallDevice = useMediaQuery('only screen and (max-width : 768px)');
-    const isMediumDevice = useMediaQuery(
-        'only screen and (min-width : 769px) and (max-width : 992px)'
-    );
-    const shouldOpen = isSmallDevice || isMediumDevice;
-    const [open, setOpen] = useState(false);
-    return (
-        <TooltipProvider delayDuration={300}>
-            <Tooltip open={open} onOpenChange={(e) => setOpen(e)}>
-                <TooltipTrigger
-                    onClick={(e) => {
-                        shouldOpen && e.preventDefault() && setOpen(true);
-                    }}
-                >
-                    <Badge variant="outline" className="px-2 mx-1 text-sm hover:cursor-pointer">
-                        {text}
-                    </Badge>
-                </TooltipTrigger>
-                <TooltipContent>{tooltip}</TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
-    );
+	const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+	const isMediumDevice = useMediaQuery(
+		"only screen and (min-width : 769px) and (max-width : 992px)",
+	);
+	const shouldOpen = isSmallDevice || isMediumDevice;
+	const [open, setOpen] = useState(false);
+	return (
+		<TooltipProvider delayDuration={300}>
+			<Tooltip open={open} onOpenChange={(e) => setOpen(e)}>
+				<TooltipTrigger
+					onClick={(e) => {
+						shouldOpen && e.preventDefault() && setOpen(true);
+					}}
+				>
+					<Badge variant="outline" className="px-2 mx-1 text-sm hover:cursor-pointer">
+						{text}
+					</Badge>
+				</TooltipTrigger>
+				<TooltipContent>{tooltip}</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	);
 }
 
-export { Tag }
+export { Tag };
--- src/pages/index/+Page.jsx
+++ src/pages/index/+Page.jsx
@@ -7,15 +7,18 @@ export default function Page() {
 				You can call me David. I'm a software engineer and previously worked at
 				<Tag text="Tencent Music" tooltip="Intern Software Developer from May 2022 to Oct 2022" />
 				and
-				<Tag text="Peking University Institute" tooltip="Junior Software Developer from Jul 2023 to Jul 2024" />
+				<Tag
+					text="Peking University Institute"
+					tooltip="Junior Software Developer from Jul 2023 to Jul 2024"
+				/>
 				.
 			</div>
 			<div>
 				In my free time, I usually
 				<Tag text="play guitar" tooltip="R&B, Neo Soul, Gospel" />
 				,and
-				<Tag text="watch movies" tooltip="Thriller, Tragicomedy" />
-				. My favorite director is Woody Allen.
+				<Tag text="watch movies" tooltip="Thriller, Tragicomedy" />. My favorite director is Woody
+				Allen.
 			</div>
 			<p>
 				Recently, I moved to Hong Kong and am seeking a software development position. Please feel

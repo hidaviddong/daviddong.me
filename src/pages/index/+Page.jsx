faf00d03891988a73b96c02ca4970daf5cdca464
daviddong <hi@daviddong.me>
2024-06-16 22:52:10 +0800

    feat: add Tag component

--- src/pages/index/+Page.jsx
+++ src/pages/index/+Page.jsx
@@ -1,61 +1,22 @@
-import { Badge } from "@/components/ui/badge";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Tag } from "@/components/ui/tag";
 export default function Page() {
 	return (
 		<article className="prose prose-p:text-neutral-500 p-12">
 			<h1>Hey, I'm Haoyu</h1>
-			<p>
+			<div>
 				You can call me David. I'm a software engineer and previously worked at
-				<TooltipProvider>
-					<Tooltip>
-						<TooltipTrigger>
-							<Badge variant="outline" className="px-2 mx-1 text-sm">
-								{/* <a href="https://www.tencentmusic.com/en-us/">Tencent Music</a> */}
-								Tencent Music
-							</Badge>
-						</TooltipTrigger>
-						<TooltipContent> Intern Software Developer from May 2022 to Oct 2022 </TooltipContent>
-					</Tooltip>
-				</TooltipProvider>
+				<Tag text="Tencent Music" tooltip="Intern Software Developer from May 2022 to Oct 2022" />
 				and
-				<TooltipProvider>
-					<Tooltip>
-						<TooltipTrigger>
-							<Badge variant="outline" className="px-2 mx-1 text-sm" asChild>
-								{/* <a href="https://icode.pku.edu.cn/">Peking University Institute</a> */}
-								Peking University Institute
-							</Badge>
-						</TooltipTrigger>
-						<TooltipContent> Junior Software Developer from Jul 2023 to Jul 2024 </TooltipContent>
-					</Tooltip>
-				</TooltipProvider>
+				<Tag text="Peking University Institute" tooltip="Junior Software Developer from Jul 2023 to Jul 2024" />
 				.
-			</p>
-			<p>
+			</div>
+			<div>
 				In my free time, I usually
-				<TooltipProvider>
-					<Tooltip>
-						<TooltipTrigger>
-							<Badge variant="outline" className="px-2 mx-1 text-sm">
-								play guitar
-							</Badge>
-						</TooltipTrigger>
-						<TooltipContent>R&B, Neo Soul, Gospel</TooltipContent>
-					</Tooltip>
-				</TooltipProvider>
+				<Tag text="play guitar" tooltip="R&B, Neo Soul, Gospel" />
 				,and
-				<TooltipProvider>
-					<Tooltip>
-						<TooltipTrigger>
-							<Badge variant="outline" className="px-2 mx-1 text-sm">
-								watch movies
-							</Badge>
-						</TooltipTrigger>
-						<TooltipContent>Thriller, Tragicomedy</TooltipContent>
-					</Tooltip>
-				</TooltipProvider>
+				<Tag text="watch movies" tooltip="Thriller, Tragicomedy" />
 				. My favorite director is Woody Allen.
-			</p>
+			</div>
 			<p>
 				Recently, I moved to Hong Kong and am seeking a software development position. Please feel
 				free to contact me!

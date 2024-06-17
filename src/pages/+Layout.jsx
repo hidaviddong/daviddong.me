2ca3e7bd87e74e288e18053c5a01380e18e417df
DongHY1 <hi@daviddong.me>
2024-06-17 17:33:32 +0800

    feat: add dock

--- src/pages/+Layout.jsx
+++ src/pages/+Layout.jsx
@@ -1,36 +1,53 @@
-// import { useState } from "react";
-// import { Button } from "@/components/ui/button";
-// import { motion } from "framer-motion";
+import { Dock, DockIcon } from "@/components/ui/dock";
+import {
+	EmailIcon,
+	GithubIcon,
+	LinkedInIcon,
+	ResumeIcon,
+	WhatsAppIcon,
+} from "@/components/ui/icons";
+import { navigate } from "vike/client/router";
 export default function LayoutDefault({ children }) {
-	// const [isOpen, setIsOpen] = useState(false);
 	return (
 		<div className="w-full h-screen selection:text-blue-500 selection:bg-blue-100 bg-dot-black/[0.2] flex flex-col items-center">
 			<main className="flex justify-center items-center flex-col">{children}</main>
-			{/* <motion.div
-				layout
-				data-isopen={isOpen}
-				initial={{ borderRadius: 50 }}
-				transition={{ duration: 0.3 }}
-				className="fixed bottom-4 data-[isopen=true]:h-48 backdrop-blur-sm w-96 h-12 flex flex-col justify-center items-center  bg-white/30 shadow-lg"
-				onHoverStart={() => setIsOpen(true)}
-				onHoverEnd={() => setIsOpen(false)}
-				onClick={() => setIsOpen(!open)}
-			>
-				<motion.div
-					layout
-					className="text-black font-sans font-medium m-1 w-full flex justify-center items-center gap-x-4"
+			<Dock className="fixed bottom-2 bg-white">
+				<DockIcon
+					onClick={() => {
+						window.location.href = "https://github.com/DongHY1";
+					}}
 				>
-					<Button variant="ghost" asChild>
-						<a href="/">About</a>
-					</Button>
-					<Button variant="ghost" asChild>
-						<a href="/blog">Blog</a>
-					</Button>
-					<Button variant="ghost" asChild>
-						<a href="/project">Project</a>
-					</Button>
-				</motion.div>
-			</motion.div> */}
+					<GithubIcon className="h-6 w-6" />
+				</DockIcon>
+				<DockIcon
+					onClick={() => {
+						window.location.href = "https://www.linkedin.com/in/DongHY1";
+					}}
+				>
+					<LinkedInIcon className="h-6 w-6" />
+				</DockIcon>
+				<DockIcon
+					onClick={() => {
+						window.location.href = "mailto:hi@daviddong.me";
+					}}
+				>
+					<EmailIcon className="h-6 w-6" />
+				</DockIcon>
+				<DockIcon
+					onClick={() => {
+						window.location.href = "https://wa.link/bbs3i9";
+					}}
+				>
+					<WhatsAppIcon className="h-6 w-6" />
+				</DockIcon>
+				<DockIcon
+					onClick={() => {
+						window.location.href = "https://typst.app/project/rIWFtNvpmsgHgh18iqpk2a";
+					}}
+				>
+					<ResumeIcon className="h-6 w-6" />
+				</DockIcon>
+			</Dock>
 		</div>
 	);
 }

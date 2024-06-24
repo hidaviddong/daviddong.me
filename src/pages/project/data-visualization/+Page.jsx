ee2cdc647e8f310038a97ee8ca24ce5055aa377c
DongHY1 <hi@daviddong.me>
2024-06-24 17:22:29 +0800

    chore: lint

--- src/pages/project/data-visualization/+Page.jsx
+++ src/pages/project/data-visualization/+Page.jsx
@@ -1,7 +1,7 @@
-import { Separator } from '@/components/ui/separator';
-import Baked from './assets/baked.jpg'
-import Model from './assets/model.png'
-import Texture from './assets/texture.png'
+import { Separator } from "@/components/ui/separator";
+import Baked from "./assets/baked.jpg";
+import Model from "./assets/model.png";
+import Texture from "./assets/texture.png";
 export default function Page() {
 	return (
 		<div className="flex flex-col">
@@ -12,11 +12,21 @@ export default function Page() {
 			<p>A series of data visualization projects.</p>
 			<div className="flex flex-col space-y-12">
 				<div className="flex flex-col justify-center items-center">
-					<iframe scrolling="no" src="https://desktop.daviddong.me" title="desktop" className="w-full h-72 border rounded-md overflow-hidden" />
+					<iframe
+						scrolling="no"
+						src="https://desktop.daviddong.me"
+						title="desktop"
+						className="w-full h-72 border rounded-md overflow-hidden"
+					/>
 					{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-					<p className="my-0 mt-4 text-sm font-medium hover:underline hover:cursor-pointer" onClick={() => {
-						window.location.href = "https://desktop.daviddong.me"
-					}}>My Desktop Website</p>
+					<p
+						className="my-0 mt-4 text-sm font-medium hover:underline hover:cursor-pointer"
+						onClick={() => {
+							window.location.href = "https://desktop.daviddong.me";
+						}}
+					>
+						My Desktop Website
+					</p>
 				</div>
 				<div className="flex flex-col justify-center items-center">
 					<img src={Model} alt="model" className="bg-gray-50 border rounded-md my-0" />
@@ -26,14 +36,18 @@ export default function Page() {
 				<div className="flex flex-col justify-center items-center">
 					<img src={Baked} alt="baked" className="bg-gray-50 border rounded-md my-0" />
 					<p className="my-0 mt-4 text-sm font-medium">Baked</p>
-					<p className="my-0 text-neutral-400 text-sm">Baking in Blender to generate textures and improve loading performance.</p>
+					<p className="my-0 text-neutral-400 text-sm">
+						Baking in Blender to generate textures and improve loading performance.
+					</p>
 				</div>
 
 				<div className="flex flex-col justify-center items-center">
 					<img src={Texture} alt="texture" className="bg-gray-50 border rounded-md my-0" />
 
 					<p className="my-0 mt-4 text-sm font-medium">Texture & Shader</p>
-					<p className="my-0 text-neutral-400 text-sm">Apply noise texture in the shader to generate a smoke effect.</p>
+					<p className="my-0 text-neutral-400 text-sm">
+						Apply noise texture in the shader to generate a smoke effect.
+					</p>
 				</div>
 				<Separator />
 			</div>

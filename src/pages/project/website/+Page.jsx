353cded732b039639cde688d8d010d09f76c674d
DongHY1 <hi@daviddong.me>
2024-06-25 11:06:25 +0800

    feat: add puppeteer desc

--- src/pages/project/website/+Page.jsx
+++ src/pages/project/website/+Page.jsx
@@ -1,4 +1,5 @@
 import DavidDongMobile from "./assets/daviddong-mobile.png";
+import Puppeteer from './assets/puppeteer.png'
 export default function Page() {
 	return (
 		<div className="flex flex-col">
@@ -25,6 +26,12 @@ export default function Page() {
 				>
 					daviddong.me
 				</p>
+			</div>
+
+			<div className="flex flex-col justify-center items-center mt-4">
+				<img src={Puppeteer} alt="puppeteer" className="bg-gray-50 border rounded-md my-0" />
+				<p className="my-0 mt-4 text-sm font-medium">Puppeteer</p>
+				<p className="my-0 text-neutral-400 text-sm">Use Puppeteer and GitHub Actions to automatically scrape the latest resume.</p>
 			</div>
 		</div>
 	);

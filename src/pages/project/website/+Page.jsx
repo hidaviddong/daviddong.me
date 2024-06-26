f50e518afaadb600777d7ef4c99e6e3d6e38fb30
DongHY1 <hi@daviddong.me>
2024-06-26 13:39:30 +0800

    fix: top await async

--- src/pages/project/website/+Page.jsx
+++ src/pages/project/website/+Page.jsx
@@ -1,43 +1,51 @@
 import { Image } from "@/components/ui/image";
+import { useState, useEffect } from "react";
 import { codeToHtml } from 'shiki'
 
 const code = `export const useImageMeta = (src) => {
-  const [dimensions, setDimensions] = useState({
-    width: undefined,
-    height: undefined,
-  });
-  useEffect(() => {
-    const fetchImageMeta = async () => {
-      if (!src) return;
-      try {
-        const response = await fetch(src);
-        const blob = await response.blob();
-        const arrayBuffer = await new Promise((resolve, reject) => {
-          const reader = new FileReader();
-          reader.onload = () => resolve(reader.result);
-          reader.onerror = () => reject(reader.error);
-          reader.readAsArrayBuffer(blob);
-        });
-        const uint8Array = new Uint8Array(arrayBuffer);
-        const meta = imageMeta(uint8Array);
-        if (meta.width && meta.height) {
-          setDimensions({ width: meta.width, height: meta.height });
-        }
-      } catch (error) {
-        console.error("Failed to load image metadata:", error);
-        setDimensions({ width: undefined, height: undefined });
-      }
-    };
-    fetchImageMeta();
-  }, [src]);
-  return dimensions;
-};`
-const html = await codeToHtml(code, {
-	lang: 'javascript',
-	theme: 'vitesse-light'
-})
+	const [dimensions, setDimensions] = useState({
+	  width: undefined,
+	  height: undefined,
+	});
+	useEffect(() => {
+	  const fetchImageMeta = async () => {
+		if (!src) return;
+		try {
+		  const response = await fetch(src);
+		  const blob = await response.blob();
+		  const arrayBuffer = await new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.onload = () => resolve(reader.result);
+			reader.onerror = () => reject(reader.error);
+			reader.readAsArrayBuffer(blob);
+		  });
+		  const uint8Array = new Uint8Array(arrayBuffer);
+		  const meta = imageMeta(uint8Array);
+		  if (meta.width && meta.height) {
+			setDimensions({ width: meta.width, height: meta.height });
+		  }
+		} catch (error) {
+		  console.error("Failed to load image metadata:", error);
+		  setDimensions({ width: undefined, height: undefined });
+		}
+	  };
+	  fetchImageMeta();
+	}, [src]);
+	return dimensions;
+  };`
 
 export default function Page() {
+	const [html, setHtml] = useState("");
+	useEffect(() => {
+		const renderShikiHtml = async () => {
+			const generatedHtml = await codeToHtml(code, {
+				lang: 'javascript',
+				theme: 'vitesse-light'
+			});
+			setHtml(generatedHtml);
+		};
+		renderShikiHtml()
+	}, [])
 	return (
 		<article className="prose p-12">
 			<div className="flex flex-col">

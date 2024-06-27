a6a25d915d7305431b630a2e2a59586314e23ae5
DongHY1 <hi@daviddong.me>
2024-06-27 12:37:24 +0800

    feat: remove font

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,10 +71,6 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-      },
-      fontFamily: {
-        'sans': ['"Inter"', ...defaultTheme.fontFamily.sans],
-        'mono': ['"DM Mono"', ...defaultTheme.fontFamily.mono]
       }
     },
   },

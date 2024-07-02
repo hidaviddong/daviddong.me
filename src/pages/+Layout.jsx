10a421b6d63e74117a650f3df138402656a456f7
David Dong <73568310+DongHY1@users.noreply.github.com>
2024-07-02 10:49:21 +0800

    feat: add cd (#79)

--- src/pages/+Layout.jsx
+++ src/pages/+Layout.jsx
@@ -1,11 +1,15 @@
 import './index.css'
+import { usePageContext } from 'vike-react/usePageContext'
 export { Layout }
 function Layout({ children }) {
 	const year = new Date().getFullYear();
+	const pageContext = usePageContext()
+	const shouldShowCD = pageContext.urlOriginal !== '/'
 	return (
 		<div className="w-full  selection:text-blue-500 selection:bg-blue-100 flex flex-col items-center">
 			<main className="h-full w-full flex justify-center items-center flex-col">{children}</main>
-			<footer className="w-full text-center text-sm text-zinc-700 relative bottom-0">
+			<footer className="w-full text-center text-sm space-y-4 text-zinc-700 relative bottom-0">
+				{shouldShowCD && <a href="/" className="font-mono underline underline-offset-1">cd /</a>}
 				<p>© {year} David Dong. All rights reserved.</p>
 			</footer>
 		</div>
